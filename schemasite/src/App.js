5984a2195de8b1fe3786d788ad1d76b630b1e3d7
Ross Blair <rosswilsonblair@gmail.com>
2018-10-18 15:10:51 -0700

    strict properties on main model schema object. Dumb styling for a silly site

--- schemasite/src/App.js
+++ schemasite/src/App.js
@@ -6,8 +6,8 @@ let schema = require('./schema.json');
 class Valid extends Component {
   render() {
     let display = [];
-    this.props.valid.split(',').map((x, i) => {
-      display.push(<li key={i}>{x}</li>);
+    this.props.valid.map((x, i) => {
+      display.push(<li key={i}><pre>{JSON.stringify(x, null, 2)}</pre></li>);
       return x;
     });
     return (
@@ -22,7 +22,7 @@ class App extends Component {
     this.state = {
       value: 'test',
       schema: schema,
-      valid: "Invalid"
+      valid: ["Invalid"]
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -42,10 +42,10 @@ class App extends Component {
     //Validation Rule
     let valid = validate(newVal);
     if (valid) {
-        valid = 'Valid!';
+        valid = ['Valid!'];
     } else {
         console.log(validate.errors);
-        valid = 'Invalid: ' + ajv.errorsText(validate.errors);
+        valid = validate.errors;
     }
     this.setState({valid: valid});
   }
@@ -53,12 +53,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          <div>
+        <div className="header">
+          <h1>BIDS Schema Validator</h1>
+        </div>
+        <div className="body">
+          <div className="left">
             <textarea
               value={this.state.value}
               onChange={this.handleChange}
             />
+          </div>
+          <div className="right">
             <Valid valid={this.state.valid}/>
+          </div>
           </div>
       </div>
     );
